e5ec10f76f4b7f2897b6c5e9afd1e07b342fea60
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2025-07-21 19:12:58 +0200

    Start using context error

--- src/inspect/page_inspect.rs
+++ src/inspect/page_inspect.rs
@@ -1,42 +1,14 @@
 use std::mem;
 
-use log::debug;
+use nom::combinator::map;
 use nom::IResult;
 use nom::number::complete::{le_u16, le_u32};
-use nom::error::{ErrorKind, ParseError};
+use nom::error::{context, ContextError, ParseError};
 use nom::Parser;
 
 use crate::xlog::common::TransactionId;
 
-#[derive(Debug)]
-pub enum InspectError<I: Sized> {
-    /// An error encountered during parsing
-    NomParseError(I, ErrorKind),
-}
-
-impl<I> ParseError<I> for InspectError<I> {
-    fn from_error_kind(input: I, kind: ErrorKind) -> Self {
-        InspectError::NomParseError(input, kind)
-    }
-    fn append(input: I, kind: ErrorKind, _other: Self) -> Self {
-        InspectError::NomParseError(input, kind)
-    }
-}
-
-impl<I> ParseError<(I, usize)> for InspectError<I> {
-    fn from_error_kind((input, _s): (I, usize), kind: ErrorKind) -> Self {
-        InspectError::NomParseError(input, kind)
-    }
-    fn append((input, _s): (I, usize), kind: ErrorKind, _other: Self) -> Self {
-        InspectError::NomParseError(input, kind)
-    }
-}
-
-impl<I> From<InspectError<I>> for nom::Err<InspectError<I>> {
-    fn from(item: InspectError<I>) -> Self {
-        nom::Err::Error(item)
-    }
-}
+type BitInput<'a> = (&'a [u8], usize);
 
 type LocationIndex = u16;
 
@@ -87,21 +59,25 @@ pub const PD_ALL_VISIBLE: u8 = 0x0004;
 /// OR of all valid pd_flags bits
 pub const PD_VALID_FLAG_BITS: u8 = 0x0007;
 
-pub fn parse_rec_ptr(i: &[u8]) -> IResult<&[u8], PageXLogRecPtr, InspectError<&[u8]>> {
-    let (i, xlogid) = le_u32(i)?;
-    let (i, xrecoff) = le_u32(i)?;
-    Ok((i, PageXLogRecPtr { xlogid, xrecoff }))
+fn parse_rec_ptr<'a, E: ParseError<&'a [u8]>+ContextError<&'a [u8]>> (
+    i: &'a [u8]
+) -> IResult<&'a [u8], PageXLogRecPtr, E> {
+    context("Xlog Rec",
+    map((le_u32, le_u32)
+        , |(xlogid, xrecoff)| { PageXLogRecPtr {xlogid, xrecoff} })
+    ).parse(i)
 }
 
-pub fn parse_item_id_data(i: &[u8]) -> IResult<&[u8], ItemIdData, InspectError<&[u8]>> {
-    debug!("Got {:?}", i);
+pub fn parse_item_id_data<'a, E: ParseError<BitInput<'a>>+ContextError<BitInput<'a>>> (
+    i: &'a [u8]
+) -> IResult<&'a [u8], ItemIdData, E> {
     let ((i, s), lp_off) = nom::bits::complete::take(15usize)((i, 0))?;
     let ((i, s), lp_flags) = nom::bits::complete::take(2usize)((i, s))?;
     let ((i, _s), lp_len) = nom::bits::complete::take(15usize)((i, s))?;
     Ok((i, ItemIdData { lp_off, lp_flags, lp_len }))
 }
 
-pub fn page_get_max_offset_number(pd_lower_u16: LocationIndex)-> usize{
+fn page_get_max_offset_number(pd_lower_u16: LocationIndex)-> usize{
     let pd_lower= usize::from(pd_lower_u16);
     // Should be 24
     let size_page_header_data = mem::offset_of!(PageHeaderData, pd_linp);
@@ -112,7 +88,9 @@ pub fn page_get_max_offset_number(pd_lower_u16: LocationIndex)-> usize{
     (pd_lower - size_page_header_data) / 4
 }
 
-pub fn parse_page_header(i: &[u8]) -> IResult<&[u8], PageHeaderData, InspectError<&[u8]>> {
+pub fn parse_page_header<'a, E: ParseError<&'a [u8]>+ContextError<&'a [u8]>> (
+    i: &'a [u8]
+) -> IResult<&'a [u8], PageHeaderData, E> {
     let (i, pd_lsn) = parse_rec_ptr(i)?;
     let (i, pd_checksum) = le_u16(i)?;
     let (i, pd_flags) = le_u16(i)?;
@@ -123,9 +101,8 @@ pub fn parse_page_header(i: &[u8]) -> IResult<&[u8], PageHeaderData, InspectErro
     let (i, pd_prune_xid) = le_u32(i)?;
 
     let num_line_pointers = page_get_max_offset_number(pd_lower);
-    debug!("Got {:?}, count {}, pd_lower {}", i, num_line_pointers, pd_lower);
-    let (i, pd_linp) = nom::multi::count(parse_item_id_data, num_line_pointers).parse(i)?;
-    debug!("Got {:?}", i);
+    let (i, pd_linp) = (i, Vec::new());
+    // let (i, pd_linp) = nom::multi::count(parse_item_id_data, num_line_pointers).parse(i)?;
 
     Ok((
         i,
